603cfcc9da18f86135aec2c0cd58c13c14cea3e8
Utako Kase <utakok@uw.edu>
2017-05-23 14:26:17 -0700

    ex1 completed

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -9,25 +9,34 @@ library(stringr)
 library(ggplot2)
 
 # Load booksinto a dataframe using the austen_books() function
-
+books <- austen_books()
 
 # How many books are in the dataset?
-
+num.books <- nrow(books)
 
 # Which book has the most lines?
+book.line <- books %>% group_by(book) %>% summarise(line = n()) %>% filter(line == max(line))
 
 
 # Use the unnest_tokens function to generate the full list of words
-
+words <- unnest_tokens(books, word, text)
 
 # Which words are most common (regardless of which book them come from)?
-
+num.words <- words %>% group_by(words) %>% summarise(count = n()) %>% arrange(-count)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
-
+no.stop.words <- words %>% anti_join(stop_words, by = "word")
 
 # Which non stop-words are most common?
-
+num.no.stop.words <- no.stop.words %>% group_by(word) %>% summarise(count = n()) %>% arrange(-count)
 
 # Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
+no.stop.words %>% 
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
 
